7c8deaa7e801e733fc8c68d721a92ef31c70467d
Hanne Kyllo Ødegård <hannekod@stud.ntnu.no>
2022-04-06 13:43:36 +0200

    fix recipe fetch and add indexing

--- src/views/Recipe.tsx
+++ src/views/Recipe.tsx
@@ -1,85 +1,83 @@
 import { useEffect, useState } from "react";
 import sanityClient from "../client";
-import Papa from "papaparse";
-import { Url } from "url";
 
 interface IRecipe {
-    recipeName: string;
-    recipeDesc: string;
-    recipeToDo: string;
-    recipePart: string;
-    recipeIngredients: {
-        ingredientsName: string;
-        quantity: string;
-        unit: string; 
-    };
-    recipeImage: { asset: { _id: string; url: string } }[];
-    recipeVideo: Url;
+  title: string;
+  recipeName: string;
+  recipeImage: { asset: { _id: string; url: string } }[];
+  recipeVideo: string;
+  recipePart: string;
+  recipeIngredients: {
+    ingredientsName: string;
+    quantity: string;
+    unit: string;
+  };
 }
 
-//const queryString = window.location.search;
-//const urlParams = new URLSearchParams(queryString);
-//const code = urlParams.get('code')
-//console.log(code);
-
 const Recipe = () => {
-    const [recipes, getRecipe] = useState<IRecipe[]>([]);
-    useEffect(() => {
-        sanityClient
-            .fetch(
-                `*[_type == "recipes"]{
-                    recipeName,
-                    recipeDesc,
-                    recipeToDo, 
-                    recipePart, 
-                    recipeIngredients[] {
-                        ingredientName, 
-                        unity,
-                        quantity
-                    },
-                    recipeImage[]{
-                        asset->{
-                            _id,
-                            url
-                        },
-                    },
-                    recipeVideo,
-                }`
-            )
-            .then((data) => getRecipe(data))
-            .catch(() => getRecipe([]));
-    }, []);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const code = urlParams.get("code") || "0";
+  console.log(code);
 
-    console.log(recipes)
+  const [recipes, setRecipes] = useState<IRecipe[]>([]);
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "recipes"]{
+                title,
+                recipeName,
+                recipeDesc,
+                recipePart,
+                recipeIngredients[] {
+                  ingredientName,
+                  unit,
+                  quantity
+                }
+              ,
+                recipeImage[]{
+                  asset->{
+                    _id,
+                    url
+                  },
+                },
+                recipeVideo,  
+            
+          }`
+      )
+      .then((data) => setRecipes(data))
+      .catch(() => setRecipes([]));
+  }, []);
 
-    let codHead = recipes[0]
-    useEffect(() => {
-        codHead = recipes[0]
-    }, [recipes])
-    
-    return (
+  console.log(recipes);
+
+  let codHead = recipes[parseInt(code)];
+  console.log(codHead);
+
+  //   useEffect(() => {
+  //     codHead = recipes[0];
+  //   }, [recipes]);
+
+  return (
     <div className="flex flex-col items-center">
-        <div className="flex items-center justify-around w-full mt-5 mb-10">
+      <div className="flex items-center justify-around w-full mt-5 mb-10">
         <a href="about-us" className="text-2xl text-black">
-            Om oss
+          Om oss
         </a>
         <a href="/">
-            <img src="Svart logo.png" alt="logo" />
+          <img src="Svart logo.png" alt="logo" />
         </a>
         <a href="/recipes" className="text-2xl text-blue-600">
-            Oppskrifter
+          Oppskrifter
         </a>
-        </div>
-
+      </div>
+      {codHead ? (
         <div>
-            <h3>
-                {codHead.recipeName}
-            </h3>
+          <h3>{codHead.recipeName}</h3>
         </div>
-
-
+      ) : null}
     </div>
-    );
+  );
 };
 
 export default Recipe;
